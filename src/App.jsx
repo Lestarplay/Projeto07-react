1a499e2019b4a5e7bb8510fef38872b7a9921ab0
Breno B.franco <162745949+Lestarplay@users.noreply.github.com>
2024-11-12 19:52:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BuscaDeFilmes from "./components/BuscaDeFilmes";
+
+function App() {
+  return(
+    <>
+    <BuscaDeFilmes/>
+    </>
+  )
+
+}
+
+
+export default App
